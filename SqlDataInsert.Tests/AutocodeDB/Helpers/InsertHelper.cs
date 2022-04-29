8d373352bc8667fd2b1291a742b60dafdd5570aa
Bohdan Dolian <Bohdan_Dolian@epam.com>
2022-04-29 11:52:16 +0300

    Fix insert helper

--- SqlDataInsert.Tests/AutocodeDB/Helpers/InsertHelper.cs
+++ SqlDataInsert.Tests/AutocodeDB/Helpers/InsertHelper.cs
@@ -5,7 +5,7 @@ namespace AutocodeDB.Helpers
     public static class InsertHelper
     {
         private static readonly Regex InsertRegExp = 
-            new Regex(@"^\s*INSERT\s+INTO\s*((\s)|(\[))(([_]+[A-Za-z0-9])|([A-Za-z]))[A-Za-z_0-9]*((\])|(\s))\s*VALUES\s*\(", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            new Regex(@"^\s*INSERT\s+INTO\s*((\s)|(\[))(([_]+[A-Za-z0-9])|([A-Za-z]))[A-Za-z_0-9]*((\])|(\s)|(\())(\s?.+\))?\s*VALUES\s*\(", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public static bool ContainsCorrectInsertInstruction(string query) => InsertRegExp.IsMatch(query);
     }
